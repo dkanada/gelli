0e39ec030ff27a308f7ad780c34bdd65ddfc0aeb
Zach Myers <zachmyers@woosterbrush.com>
2020-10-20 16:17:31 -0400

    adjust to proper preference value

--- app/src/main/java/com/dkanada/gramophone/util/PlaylistUtil.java
+++ app/src/main/java/com/dkanada/gramophone/util/PlaylistUtil.java
@@ -19,7 +19,7 @@ import java.util.List;
 public class PlaylistUtil {
     public static void getPlaylist(PlaylistItemQuery query, MediaCallback callback) {
         query.setUserId(App.getApiClient().getCurrentUserId());
-        query.setLimit(PreferenceUtil.getInstance(App.getInstance()).getMaximumListSize());
+        query.setLimit(PreferenceUtil.getInstance(App.getInstance()).getPageSize());
         App.getApiClient().GetPlaylistItems(query, new Response<ItemsResult>() {
             @Override
             public void onResponse(ItemsResult result) {
